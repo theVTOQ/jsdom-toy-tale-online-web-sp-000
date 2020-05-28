065d313aa1d58227309da67414b1449cfaaa1125
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-28 23:09:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function renderToys(toys){
     for(let toy in toys) {
       console.log(`renderToys: 1.${toy[0]} 2.${toy[0]}`);
+//      const toy = fetchIndividualToy(toyId);
 
       toyFormContainer.appendChild(createToyCard(toy));
     }
